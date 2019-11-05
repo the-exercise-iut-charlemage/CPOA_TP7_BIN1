48e1be5caf5eb1579dfdd2f5f769cbab6e09356a
Safyrus <54881649+Safyrus@users.noreply.github.com>
2019-11-05 09:33:16 +0100

    modif test getInstance

--- test/activeRecord/DBConnectionTest.java
+++ test/activeRecord/DBConnectionTest.java
@@ -1,20 +1,22 @@
 package activeRecord;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DBConnectionTest {
 
-    @org.junit.Test
+    @Test
     public void getInstance() {
         boolean res = true;
         DBConnection d1 = DBConnection.getInstance();
         DBConnection d2 = DBConnection.getInstance();
-        if(d1 == d2)
+        if(d1 != d2)
             res = false;
         assertTrue("plusieur instance de DBConnection", res);
     }
 
-    @org.junit.Test
+    @Test
     public void getConnection() {
         boolean res = true;
         DBConnection d = DBConnection.getInstance();
@@ -24,7 +26,7 @@ public class DBConnectionTest {
         assertTrue("getInstance ne retourne pas le bon type", res);
     }
 
-    @org.junit.Test
+    @Test
     public void setNomDB() {
         DBConnection d = DBConnection.getInstance();
         java.sql.Connection c = d.getConnection();
