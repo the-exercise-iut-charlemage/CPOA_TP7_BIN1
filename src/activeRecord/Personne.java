f7cf7addc1f7b40e8a50ee849b2a57cd1df4184a
Safyrus <54881649+Safyrus@users.noreply.github.com>
2019-11-18 22:04:57 +0100

    add createTable and deleteTable in Personne

--- src/activeRecord/Personne.java
+++ src/activeRecord/Personne.java
@@ -98,4 +98,26 @@ public class Personne {
         }
         return personnes;
     }
+
+    public static void createTable(){
+        DBConnection dbConnection = DBConnection.getInstance();
+        Connection connection = dbConnection.getConnection();
+        try {
+            PreparedStatement statement = connection.prepareStatement("drop table if exist 'personne'");
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteTable(){
+        DBConnection dbConnection = DBConnection.getInstance();
+        Connection connection = dbConnection.getConnection();
+        try {
+            PreparedStatement statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS 'personne' ('ID' int(11) NOT NULL AUTO_INCREMENT,'NOM' varchar(40) NOT NULL,'PRENOM' varchar(40) NOT NULL,PRIMARY KEY ('ID')) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5");
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
