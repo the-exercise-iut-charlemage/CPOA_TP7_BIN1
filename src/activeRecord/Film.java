1dc047d0decb3c8fe7e0f38de0c1b089f08d5b85
Safyrus <54881649+Safyrus@users.noreply.github.com>
2019-11-19 08:40:38 +0100

    add Film

---
+++ src/activeRecord/Film.java
@@ -0,0 +1,47 @@
+package activeRecord;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class Film {
+    private String titre;
+    private int id;
+    private int id_real;
+
+    public Film(String t, Personne p){
+        titre = t;
+        id_real = p.getId();
+        id = -1;
+    }
+
+    private Film(String titre, int id, int id_real){
+        this.titre = titre;
+        this.id = id;
+        this.id_real = id_real;
+    }
+
+    public static Film findById(int id){
+        Film film = null;
+        DBConnection dbConnection = DBConnection.getInstance();
+        Connection connection = dbConnection.getConnection();
+        try {
+            PreparedStatement statement = connection.prepareStatement("select * from film where id=?");
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if(resultSet.next()){
+                Film f = new Film(resultSet.getString("TITRE"), resultSet.getInt("ID"), resultSet.getInt("ID_REA"));
+                film = f;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return film;
+    }
+
+    public Personne getRealisateur(){
+        return Personne.findById(id_real);
+    }
+}
